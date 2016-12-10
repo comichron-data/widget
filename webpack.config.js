4a9fe2c4763d644c83787e9c0691306e794f3eb7
Paul Salaets <psalaets@gmail.com>
2016-12-10 15:08:36 -0500

    getting webpack set up for using sass instead of inline styles

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,11 @@ module.exports = {
   entry: './src/entry.js',
   output: {
     filename: 'script.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass']
+    }]
   }
 };
